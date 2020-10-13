ae26431c79f6fdb4b88543607fefec5dc9f1118c
Tomoyasu <tms.iwasawa@gmail.com>
2020-10-14 00:49:56 +0900

    Added ParticleSystem on 10/14/2020

--- Unity2019_Script/Chap4/MySampleProject4/Assets/Scripts/MyScript.cs
+++ Unity2019_Script/Chap4/MySampleProject4/Assets/Scripts/MyScript.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.Rendering;
 
 /// <summary>
@@ -9,21 +11,27 @@ public class MyScript : MonoBehaviour
 {
     string cubeStr = "Cube";
     string playerStr = "Player";
+    string gameObjectStr = "GameObject";
     int counter = 0;
     GameObject obj = null;
+    GameObject[] cubes = new GameObject[4];
+    GameObject[] gos = new GameObject[4];
 
     // Start is called before the first frame update
     void Start()
     {
-        Start4_5();
+        // Start4_5();
+        // Start4_6();
+        Start4_8();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Method4_1();
-        // Method4_3();
-        Method4_4_5();
+        // Update4_1();
+        // Update4_3();
+        // Update4_4_5_6_7();
+        Update4_8();
     }
 
     /// <summary>
@@ -36,7 +44,7 @@ public class MyScript : MonoBehaviour
         // OnCollisionEnter4_2(collision);
         // OnCollisionEnter4_3(collision);
         // OnCollisionEnter4_4(collision);
-        OnCollisionEnter4_5(collision);
+        // OnCollisionEnter4_5(collision);
     }
 
     /// <summary>
@@ -46,13 +54,34 @@ public class MyScript : MonoBehaviour
     private void OnCollisionExit(Collision collision)
     {
         // OnCollisionExit4_4(collision);
-        OnCollisionExit4_5(collision);
+        // OnCollisionExit4_5(collision);
+    }
+
+    /// <summary>
+    /// Event of trigger enter
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerEnter(Collider collider)
+    {
+        // OnTriggerEnter4_6(collider);
+        // OnTriggerEnter4_7(collider);
+        OnTriggerEnter4_8(collider);
+    }
+
+    /// <summary>
+    /// Event of trigger exit
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerExit(Collider collider)
+    {
+        // OnTriggerExit4_6(collider);
+        // OnTriggerExit4_7(collider);
     }
 
     /// <summary>
     /// Collistion Event
     /// </summary>
-    private void Method4_1()
+    private void Update4_1()
     {
         GameObject cube = GameObject.Find(cubeStr);
         Rigidbody rigidbody = GetComponent<Rigidbody>();
@@ -73,7 +102,7 @@ public class MyScript : MonoBehaviour
     /// <summary>
     /// Delete Object
     /// </summary>
-    private void Method4_3()
+    private void Update4_3()
     {
         GameObject cube = GameObject.Find(cubeStr);
         Rigidbody rigidbody = GetComponent<Rigidbody>();
@@ -112,7 +141,7 @@ public class MyScript : MonoBehaviour
     /// <param name="collision"></param>
     private void OnCollisionEnter4_4(Collision collision)
     {
-        if (collision.gameObject.name == cubeStr) ChangeMaterialColor(collision, 1f, 0, 0, 0.25f);
+        if (collision.gameObject.name == cubeStr) ChangeMaterialColor(collision.gameObject, 1f, 0, 0, 0.25f);
     }
 
     /// <summary>
@@ -121,7 +150,7 @@ public class MyScript : MonoBehaviour
     /// <param name="collision"></param>
     private void OnCollisionExit4_4(Collision collision)
     {
-        if (collision.gameObject.name == cubeStr) ChangeMaterialColor(collision, 1f, 0, 0, 1f);
+        if (collision.gameObject.name == cubeStr) ChangeMaterialColor(collision.gameObject, 1f, 0, 0, 1f);
     }
 
     /// <summary>
@@ -147,19 +176,19 @@ public class MyScript : MonoBehaviour
     /// <summary>
     /// Make cube transparent
     /// </summary>
-    private void Method4_4_5()
+    private void Update4_4_5_6_7()
     {
-        var gameObj = GetGameObjectAndRigidbody(cubeStr);
+        var obj = GetGameObjectAndRigidbody(cubeStr);
         // var gameObj = (cube: GameObject.Find(cubeStr), rigidbody: GetComponent<Rigidbody>());
         //GameObject cube = GameObject.Find(cubeStr);
         //Rigidbody rigidbody = GetComponent<Rigidbody>();
         try
         {
-            SetTransformRotate(gameObj.cube, 1f, -1f, -1f);
+            SetTransformRotate(obj.gameObject, 1f, -1f, -1f);
         }
         catch (NullReferenceException e) { }
 
-        MoveByKeys(gameObj.rigidbody);
+        MoveByKeys(obj.rigidbody);
     }
 
     /// <summary>
@@ -193,11 +222,125 @@ public class MyScript : MonoBehaviour
     }
 
     /// <summary>
+    /// Start method for Trigger Event
+    /// </summary>
+    private void Start4_6()
+    {
+        GameObject cube = GameObject.Find(cubeStr);
+        cube.GetComponent<Renderer>().material.color = new Color(1f, 0, 0, 0.5f);
+    }
+
+    /// <summary>
+    /// Make cube green color
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerEnter4_6(Collider collider)
+    {
+        if (collider.gameObject.CompareTag(playerStr)) ChangeMaterialColor(collider.gameObject, 0, 1, 0, 0.5f);
+    }
+
+    /// <summary>
+    /// Make cube normal color
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerExit4_6(Collider collider)
+    {
+        if (collider.gameObject.CompareTag(playerStr)) ChangeMaterialColor(collider.gameObject, 1, 0, 0, 0.5f);
+    }
+
+    /// <summary>
+    /// Event of trigger enter for Velocity / AngularVelocity
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerEnter4_7(Collider collider)
+    {
+        if (collider.gameObject.CompareTag(playerStr))
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.angularVelocity = Vector3.zero;
+            rigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    /// <summary>
+    /// Event of trigger exit for Velocity / AngularVelocity
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerExit4_7(Collider collider)
+    {
+        if (collider.gameObject.CompareTag(playerStr))
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            Vector3 v = rigidbody.velocity;
+            v.x *= 25;
+            v.y *= 25;
+            v.z *= 25;
+            rigidbody.velocity = v;
+
+            Vector3 v2 = rigidbody.angularVelocity;
+            v2.x *= 25;
+            v2.y *= 25;
+            v2.z *= 25;
+            rigidbody.angularVelocity = v2;
+        }
+    }
+
+    /// <summary>
+    /// Start method for ParticleSystem
+    /// </summary>
+    private void Start4_8()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            cubes[i] = GameObject.Find(cubeStr + i);
+            gos[i] = GameObject.Find(gameObjectStr + i);
+        }
+    }
+
+    /// <summary>
+    /// Update method for ParticleSystem
+    /// </summary>
+    private void Update4_8()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        foreach (var obj in cubes)
+        {
+            SetTransformRotate(obj, 1f, 1f, 1f);
+        }
+        
+        Vector3 v = transform.position;
+        v.y += 2;
+        v.z -= 7;
+        Camera.main.transform.position = v;
+        MoveByKeysForParticlelSystem(rigidbody);
+    }
+
+    /// <summary>
+    /// Event of trigger enter for ParticleSystem
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerEnter4_8(Collider collider)
+    {
+        if (collider.gameObject.name.StartsWith(cubeStr))
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (cubes[i] == collider.gameObject)
+                {
+                    ParticleSystem ps = gos[i].GetComponent<ParticleSystem>();
+                    ps.Play();
+                    cubes[i].SetActive(false);
+                }
+            }
+        }
+    }
+
+    /// <summary>
     /// Tuple of Initialization for GameObject and Rigidbody
     /// </summary>
     /// <param name="gameObjStr"></param>
     /// <returns></returns>
-    private (GameObject cube, Rigidbody rigidbody) GetGameObjectAndRigidbody(String gameObjStr)
+    private (GameObject gameObject, Rigidbody rigidbody) GetGameObjectAndRigidbody(String gameObjStr)
     {
         return (GameObject.Find(gameObjStr), GetComponent<Rigidbody>());
     }
@@ -212,6 +355,18 @@ public class MyScript : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow)) SetRigidbodyAddForm(rigidbody, 1f, 0, -1f);
         if (Input.GetKey(KeyCode.UpArrow)) SetRigidbodyAddForm(rigidbody, 1f, 0, 1f);
         if (Input.GetKey(KeyCode.DownArrow)) SetRigidbodyAddForm(rigidbody, -1f, 0, -1f);
+    }
+
+    /// <summary>
+    /// Move by keys on keyboard for ParticleSystem
+    /// </summary>
+    /// <param name="rigidbody"></param>
+    private void MoveByKeysForParticlelSystem(Rigidbody rigidbody)
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) SetRigidbodyAddForm(rigidbody, -1f, 0, 0f);
+        if (Input.GetKey(KeyCode.RightArrow)) SetRigidbodyAddForm(rigidbody, 1f, 0, 0f);
+        if (Input.GetKey(KeyCode.UpArrow)) SetRigidbodyAddForm(rigidbody, 0, 0, 1f);
+        if (Input.GetKey(KeyCode.DownArrow)) SetRigidbodyAddForm(rigidbody, 0f, 0, -1f);
     }
 
     /// <summary>
@@ -246,8 +401,8 @@ public class MyScript : MonoBehaviour
     /// <param name="g"></param>
     /// <param name="b"></param>
     /// <param name="a">Alpha channel; Transparent value. 0 is complete transparent. 1 is complete opaque.</param>
-    private void ChangeMaterialColor(Collision collision, float r, float g, float b, float a)
+    private void ChangeMaterialColor(GameObject gameObject, float r, float g, float b, float a)
     {
-        collision.gameObject.GetComponent<Renderer>().material.color = new Color(r, g, b, a);
+        gameObject.GetComponent<Renderer>().material.color = new Color(r, g, b, a);
     }
 }
