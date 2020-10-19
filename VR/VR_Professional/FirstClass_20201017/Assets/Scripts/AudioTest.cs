661ba28fb166e84b4ad910e35afdc86624d69e03
Tomoyasu <tms.iwasawa@gmail.com>
2020-10-20 00:18:16 +0900

    Added the first VR project in VR Professional Academy on 10/17/2020

---
+++ VR/VR_Professional/FirstClass_20201017/Assets/Scripts/AudioTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    AudioSource audioSource;
+    [SerializeField]    // よそからアクセスできない
+    AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) audioSource.PlayOneShot(audioClip);
+    }
+
+    public void KeyPush(int key)
+    {
+        switch (key)
+        {
+            case 0:
+                audioSource.PlayOneShot(audioClip);
+                break;
+            case 1:
+                // audioSource.PlayOneShot(audioClip[i]);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ VR/VR_Professional/FirstClass_20201017/Assets/Scripts/Test.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Vector3 defaultVec;
+    public AudioClip sound;
+    public Text score;
+    int scorePoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultVec = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) rigidbody.velocity = new Vector3(0f, 10f, 0f);
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            rigidbody.velocity = Vector3.zero;
+            transform.position = defaultVec;
+        }
+        else if (Input.GetKey(KeyCode.Return)) rigidbody.velocity = rigidbody.velocity * 0.50f;
+
+        MoveByKeys();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+        // GetComponent<Audio();Source>().Pla
+        if (collision.gameObject.name.StartsWith("Cube"))
+        {
+            GetScore(collision.gameObject);
+            DeleteCube(collision.gameObject);
+        }
+    }
+
+    private void GetScore(GameObject gameObject)
+    {
+        scorePoint += 100;
+        Debug.Log(scorePoint);
+        score.text = "Point:" + scorePoint;
+    }
+
+    private void DeleteCube(GameObject gameObject)
+    {
+        Destroy(gameObject, 0f);
+    }
+
+    private void MoveByKeys()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) SetRigidbodyAddForm(-1f, 0, 1f);
+        if (Input.GetKey(KeyCode.RightArrow)) SetRigidbodyAddForm(1f, 0, -1f);
+        if (Input.GetKey(KeyCode.UpArrow)) SetRigidbodyAddForm(1f, 0, 1f);
+        if (Input.GetKey(KeyCode.DownArrow)) SetRigidbodyAddForm(-1f, 0, -1f);
+    }
+
+    //public void KeyPush(KeyCode keyCode)
+    //{
+    //    switch (keyCode)
+    //    {
+    //        case KeyCode.LeftArrow:
+    //            SetRigidbodyAddForm(-1f, 0, 1f);
+    //            break;
+    //        case 1:
+    //            // audioSource.PlayOneShot(audioClip[i]);
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    //}
+
+    private void SetRigidbodyAddForm(float x, float y, float z)
+    {
+        rigidbody.AddForce(new Vector3(x, y, z));
+    }
+}
