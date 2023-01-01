6b2ef9fa5a24fa2a18838d0225eccd62bb60a8ed
Tomoyasu <tms.iwasawa@gmail.com>
2023-01-02 02:29:09 +0900

    Added enemy features

--- Unity2020/BasicFPSGame/Assets/MainApp/Scripts/Arrow.cs
+++ Unity2020/BasicFPSGame/Assets/MainApp/Scripts/Arrow.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     #region Variables
+    /// <summary>
+    /// Attack power
+    /// </summary>
+    public int Attack = 1;
+    /// <summary>
+    /// Auto-destructive coroutine
+    /// </summary>
+    private Coroutine autoDestroyCor = null;
+    /// <summary>
+    /// Rigidbody
+    /// </summary>
     private Rigidbody rigid = null;
     /// <summary>
     /// Flag for attack
@@ -29,15 +40,17 @@ public class Arrow : MonoBehaviour
         rigid.useGravity = false;
         rigid.isKinematic = false;
         rigid.AddForce(direction, forceMode);
-        StartCoroutine(AutoDestroy(1f));
+        // StartCoroutine(AutoDestroy(1f));
+        autoDestroyCor = StartCoroutine(AutoDestroy(1f));
     }
     /// <summary>
     /// Collider enter
     /// </summary>
     /// <param name="collision"></param>
-    public void OnArrowColliderEnter(Collision collision)
+    public void OnArrowCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag.Equals("Ground") || collision.gameObject.tag.Equals("Enemy"))
+        //if (collision.gameObject.tag.Equals("Ground") || collision.gameObject.tag.Equals("Enemy"))
+        if (collision.gameObject.tag.Equals("Ground"))
         {
             if (isAttack)
             {
@@ -45,10 +58,22 @@ public class Arrow : MonoBehaviour
                 rigid.velocity = Vector3.zero;
                 rigid.angularVelocity = Vector3.zero;
                 isAttack = false;
-                // Destroy(gameObject);
+                Destroy(gameObject);
                 Debug.Log("Ground or Enemy!!! " + collision.gameObject.name);
             }
         }
+    }
+    /// <summary>
+    /// Handling enemy hits
+    /// </summary>
+    public void OnEnemyHit()
+    {
+        if (autoDestroyCor != null) StopCoroutine(autoDestroyCor);
+        rigid.isKinematic = true;
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
+        isAttack = false;
+        Destroy(gameObject);
     }
     /// <summary>
     /// Initialization
--- Unity2020/BasicFPSGame/Assets/MainApp/Scripts/EnemyBase.cs
+++ Unity2020/BasicFPSGame/Assets/MainApp/Scripts/EnemyBase.cs
@@ -8,16 +8,79 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     #region Variables
-
+    #region SerializeField
+    /// <summary>
+    /// Maximum HP
+    /// </summary>
+    [SerializeField]
+    protected int maxHp = 3;
+    #endregion
+    /// <summary>
+    /// Current HP
+    /// </summary>
+    protected int hp = 0;
+    /// <summary>
+    /// Flag for being attacked
+    /// </summary>
+    private bool canHit = true;
     #endregion
 
-    void Start()
+    protected virtual void Start()
+    {
+        Init();
+    }
+
+    protected virtual void Update()
     {
         
     }
-
-    void Update()
+    /// <summary>
+    /// Initialization
+    /// </summary>
+    public virtual void Init()
     {
-        
+        hp = maxHp;
+    }
+    /// <summary>
+    /// Collider enter process
+    /// </summary>
+    /// <param name="collision"></param>
+    public virtual void OnEnemyColliderEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Arrow") && canHit)
+        {
+            // Acquire Arrow and perform processing when hit by an "Arrow" enemy
+            var arrow = collision.gameObject.GetComponent<Arrow>();
+            arrow.OnEnemyHit();
+            // HP minus the attack power of the arrow
+            hp -= arrow.Attack;
+            // Dead process
+            if (hp <= 0) OnDead();
+            else
+            {
+                Debug.Log("Hit " + gameObject.name + ". HP is " + hp + " left.");
+                // Waiting time for next hit
+                StartCoroutine(HitWait());
+            }
+        }
+    }
+    /// <summary>
+    /// Dead process
+    /// </summary>
+    protected virtual void OnDead()
+    {
+        Destroy(gameObject);
+        Debug.Log(gameObject.name + " is beaten.");
+    }
+    /// <summary>
+    /// Waiting process after an attack hit until the next attack hits
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator HitWait()
+    {
+        // Wait for specified time and return flag
+        canHit = false;
+        yield return new WaitForSeconds(0.5f);
+        canHit = true;
     }
 }
