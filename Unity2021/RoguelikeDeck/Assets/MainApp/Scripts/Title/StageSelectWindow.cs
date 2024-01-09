6bb8cfee2966bc81c194e98ebb85cf8039472341
Tom <tms.iwasawa@gmail.com>
2024-01-10 01:58:06 +0900

    Created Title Scene on 1/10/2024

--- Unity2021/RoguelikeDeck/Assets/MainApp/Scripts/Title/StageSelectWindow.cs
+++ Unity2021/RoguelikeDeck/Assets/MainApp/Scripts/Title/StageSelectWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 /// <summary>
@@ -136,13 +137,77 @@ public class StageSelectWindow : MonoBehaviour
         // Disable window background panel
         titleManager.SetWindowBackPanelActive(false);
     }
+    /// <summary>
+    /// Button to switch to the stage one left (minus direction)
+    /// </summary>
+    public void LeftScrollButton()
+    {
+        // Selection Stage Switching
+        selectStageID--;
+        if (selectStageID < 0) selectStageID = stageListNum - 1;
+        // Show selected stage information
+        ShowStageData();
+    }
+    /// <summary>
+    /// Button to switch to one stage to the right (positive direction)
+    /// </summary>
+    public void RightScrollButton()
+    {
+        // Selection Stage Switching
+        selectStageID++;
+        if (selectStageID >= stageListNum) selectStageID = 0;
+        // Show selected stage information
+        ShowStageData();
+    }
+    /// <summary>
+    /// Stage start button
+    /// </summary>
+    /// <param name="isWithTutorial">true: Start with tutorial</param>
+    public void StageStartButton(bool isWithTutorial)
+    {
+        // Keep selected stage number
+        Data.instance.nowStageID = selectStageID;
+        // Scene switching
+        SceneManager.LoadScene("BattleScene");
+    }
+
     #endregion Public Methods
 
     #region Private Methods
 
+    /// <summary>
+    /// Show information on the currently selected stage
+    /// </summary>
     private void ShowStageData()
     {
-
+        var stageData = Data.instance.stageSOs[selectStageID];
+        // UI settings by language
+        if (Data.nowLanguage == SystemLanguage.Japanese)
+        {
+            // Stage name Text
+            stageNameText.text = stageData.nameJP;
+            // Stage difficulty Text
+            stageDifficultyText.text = stageData.difficultyJP;
+            // The number of enemy Text
+            battleNumText.text = "敵の数" + stageData.appearEnemyTables.Count;
+        }
+        else if (Data.nowLanguage == SystemLanguage.English)
+        {
+            // Stage name Text
+            stageNameText.text = stageData.nameEN;
+            // Stage difficulty Text
+            stageDifficultyText.text = stageData.difficultyEN;
+            // The number of enemy Text
+            battleNumText.text = stageData.appearEnemyTables.Count + " enemies";
+        }
+        // Stage icon Image
+        stageIconImage.sprite = stageData.stageIcon;
+        // Stage number Images
+        for (int i = 0; i < stageListNum; i++)
+        {
+            if (i == selectStageID) stageOrderImages[i].transform.localScale = new Vector2(1.0f, 1.0f);
+            else stageOrderImages[i].transform.localScale = new Vector2(0.4f, 0.4f);
+        }
     }
 
     #endregion Private Methods
