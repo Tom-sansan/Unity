fc0fe5fd297c9f822019d17459c242743dd99811
Tom <tms.iwasawa@gmail.com>
2024-01-23 01:52:21 +0900

    Creating ShoppingWindow on 1/23/2024

--- Unity2021/RoguelikeDeck/Assets/MainApp/Scripts/Title/ShoppingWindow/ShoppingWindow.cs
+++ Unity2021/RoguelikeDeck/Assets/MainApp/Scripts/Title/ShoppingWindow/ShoppingWindow.cs
@@ -1,6 +1,8 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Shopping Window Class
@@ -19,6 +21,32 @@ public class ShoppingWindow : MonoBehaviour
 
     #region SerializeField
 
+    /// <summary>
+    /// Scrollbar in ScrollView
+    /// </summary>
+    [SerializeField]
+    private Scrollbar scrollbar = null;
+    /// <summary>
+    /// Text of Amount of possession gold
+    /// </summary>
+    [SerializeField]
+    private Text goldText = null;
+    /// <summary>
+    /// List of card packs of products
+    /// </summary>
+    [SerializeField]
+    private List<CardPackSO> cardPackSOs = null;
+    /// <summary>
+    /// Product Item UI Prefab
+    /// </summary>
+    [SerializeField]
+    private GameObject shoppingItemPrefab = null;
+    /// <summary>
+    /// Parent Transform of the product item UI
+    /// </summary>
+    [SerializeField]
+    private Transform shoppingItemsParent = null;
+
     #endregion SerializeField
 
     #region Public Variables
@@ -26,6 +54,31 @@ public class ShoppingWindow : MonoBehaviour
     #endregion Public Variables
 
     #region Private Variables
+
+    /// <summary>
+    /// Window display animation time
+    /// </summary>
+    private const float WindowAnimTime = 0.3f;
+    /// <summary>
+    /// Title management class
+    /// </summary>
+    private TitleManager titleManager;
+    /// <summary>
+    /// RectTransform of window
+    /// </summary>
+    private RectTransform windowRectTransform = null;
+    /// <summary>
+    /// List of generated product items
+    /// </summary>
+    private List<ShoppingItem> shoppingItemInstances;
+    /// <summary>
+    /// Window show/hide Tween
+    /// </summary>
+    private Tween windowTween;
+    /// <summary>
+    /// Scrollbar initialization flag
+    /// </summary>
+    private bool reserveResetScrollBar;
 
     #endregion Private Variables
 
@@ -46,9 +99,51 @@ public class ShoppingWindow : MonoBehaviour
 
     #region Public Methods
 
+    /// <summary>
+    /// Initialization (called from TitleManager.cs)
+    /// </summary>
+    /// <param name="titleManager"></param>
+    public void Init(TitleManager titleManager)
+    {
+        this.titleManager = titleManager;
+        windowRectTransform = GetComponent<RectTransform>();
+        shoppingItemInstances = new List<ShoppingItem>();
+        // TODO
+#if UNITY_EDITOR
+        // For debug: Add gold coins
+        Data.instance.ChangePlayerGold(100000);
+#endif
+        // Create product item
+        for (int i = 0; i < cardPackSOs.Count; i++)
+        {
+            // Create item
+            var obj = Instantiate(shoppingItemPrefab, shoppingItemsParent);
+            var shoppingItem = obj.GetComponent<ShoppingItem>();
+            // Set item
+            shoppingItemInstances.Add(shoppingItem);
+        }
+        // Display of the amount of Gold in possession
+        goldText.text = Data.instance.playerGold.ToString("#,0");
+        // Hide window
+        windowRectTransform.transform.localScale = Vector2.zero;
+        windowRectTransform.gameObject.SetActive(true);
+    }
     #endregion Public Methods
 
     #region Private Methods
+
+    /// <summary>
+    /// OnGUI
+    /// </summary>
+    private void OnGUI()
+    {
+        // 
+        if (reserveResetScrollBar)
+        {
+            scrollbar.value = 1.0f;
+            reserveResetScrollBar = false;
+        }
+    }
 
     #endregion Private Methods
 
