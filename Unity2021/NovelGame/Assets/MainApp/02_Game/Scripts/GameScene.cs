c3a8802d24f13da8a6583071d5be0c097ab6104e
Tom <tms.iwasawa@gmail.com>
2024-03-03 03:17:00 +0900

    First commit on 3/3/2024

---
+++ Unity2021/NovelGame/Assets/MainApp/02_Game/Scripts/GameScene.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : SceneBase
+{
+    #region Class
+
+    #endregion Class
+
+    #region Enum
+
+    #endregion Enum
+
+    #region Variables
+
+    #region SerializeField
+
+    #endregion SerializeField
+
+    #region Protected Variables
+
+    #endregion Protected Variables
+
+    #region Public Variables
+
+    #endregion Public Variables
+
+    #region Private Variables
+
+    #endregion Private Variables
+
+    #endregion Variables
+
+    #region Methods
+
+    #region Unity Methods
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    #endregion Unity Methods
+
+    #region Public Methods
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    #endregion Private Methods
+
+    #endregion Methods
+}
---
+++ Unity2021/NovelGame/Assets/MainApp/02_Game/Scripts/GameView.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Game View class
+/// </summary>
+public class GameView : ViewBase
+{
+    #region Class
+
+    #endregion Class
+
+    #region Enum
+
+    #endregion Enum
+
+    #region Variables
+
+    #region SerializeField
+
+    #endregion SerializeField
+
+    #region Protected Variables
+
+    #endregion Protected Variables
+
+    #region Public Variables
+
+    #endregion Public Variables
+
+    #region Private Variables
+
+    #endregion Private Variables
+
+    #endregion Variables
+
+    #region Methods
+
+    #region Unity Methods
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    #endregion Unity Methods
+
+    #region Public Methods
+
+    /// <summary>
+    /// Call at view open
+    /// </summary>
+    public override void OnViewOpened()
+    {
+        base.OnViewOpened();
+    }
+    /// <summary>
+    /// Call at view close
+    /// </summary>
+    public override void OnViewClosed()
+    {
+        base.OnViewClosed();
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    #endregion Private Methods
+
+    #endregion Methods
+}
